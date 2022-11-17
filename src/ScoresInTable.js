f1d69755d3f059c20a3fe61d5dd99d3c4e637f37
Kawa MANMI <kawamam2007@gmail.com>
2022-11-17 00:32:30 +0000

    Level 3  in high score project  has been done

--- src/ScoresInTable.js
+++ src/ScoresInTable.js
@@ -1,7 +1,7 @@
 import React from "react";
 import allCountriesData from "./scoresData";
 export default function ScoresInTable() {
-  function compare(a, b) {
+  function sortAccordingToName(a, b) {
     if (a.name < b.name) {
       return -1;
     }
@@ -10,7 +10,24 @@ export default function ScoresInTable() {
     }
     return 0;
   }
-  allCountriesData.sort(compare);
+
+  function sortAccordingToName2(a, b) {
+    if (a.s < b.s) {
+      return -1;
+    }
+    if (a.s > b.s) {
+      return 1;
+    }
+    return 0;
+  }
+  function sortAccordingToScores() {
+    allCountriesData.map((countryData) =>
+      countryData["scores"].sort(sortAccordingToName2)
+    );
+  }
+  allCountriesData.sort(sortAccordingToName);
+  sortAccordingToScores();
+
   return (
     <div>
       {allCountriesData.map((countryData) => (
